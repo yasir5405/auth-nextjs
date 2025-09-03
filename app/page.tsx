d969038a3ef9aab3f6f1c07a3eac7c7c077cc98d
yasir5405 <yasirnaseem5415@gmail.com>
2025-09-04 04:40:08 +0530

    add server-side fetching for a component without writing extra api endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <h1>Hello</h1>;
+import { client } from "../lib/prisma";
+
+const fetchUser = async () => {
+  try {
+    const user = await client.users.findFirst({});
+    return {
+      name: user?.name,
+      email: user?.email,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function Home() {
+  const userData = await fetchUser();
+  return (
+    <div>
+      <h1>Name: {userData?.name}</h1>
+      <h1>Name: {userData?.email}</h1>
+    </div>
+  );
 }
